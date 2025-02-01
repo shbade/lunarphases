890d0adb80401317be563d8e25b80611faed8419
shayne <smb2ev@mtmail.mtsu.edu>
2025-02-01 09:17:47 -0600

    added most of the stuff for the js file and began working on beautifying the html

--- script.js
+++ script.js
@@ -1,28 +1,72 @@
-let yearform = document.getElementById("user_input");
-yearform.addEventListener("submit", (e) => {
-    e.preventDefault;
-
-const date = new Date();
-console.log(date);
-
-// These will be the variables that store the provided date elements, which are put into 
-// the givenDate variable to provide the difference between the dates
-
-// Take the given date and store respective values into the variables below
-
-let givenYear = document.getElementById("year"); // year element
-givenYear = parseInt(givenYear);
-let givenMonth = document.getElementById("month");// month element
-givenMonth = parseInt(givenMonth);
-let givenDay = document.getElementById("day");// day element
-givenDay = parseInt(givenDay);
+// Additions could include a function that prints how many phases will have passed at a future 
+// given date.
 
 
-// originFullMoon is for the date from which we perform calculations. this is the time of 
-// a known full moon and where the cycle begins for counting cycles
-const originFullMoon = new Date ("01-13-2025T11:59:59");
-const givenDate = new Date (givenYear, givenMonth, givenDay);
+let millisecondsToDays = 1000 * 60 * 60 * 24; // converts time given by js into days
+const LENGTHOFCYCLE = 29.5; //number of days in a full moon cycle
+
+let anchorDate = new Date(1-13-2025) // the date of the last know full moon used to find future dates
+let userDate = prompt("Enter date in YYYY-MM-DD format.")
+const originDate = '2025-01-13';
+let cycleRemainder;
+let phase;
 
 
-console.log(givenDate);
-});
+function calculateDifferenceinDays(originDate, userDate) {
+    // convert the dates to milliseconds
+    let anchor = new Date(originDate).getTime();
+    let chosenDate = new Date(userDate).getTime();
+    
+    // calculate the difference in milliseconds
+    let differenceInMillis = chosenDate - anchor;
+    
+    // Convert the difference from milliseconds to days
+    let differenceInDays = differenceInMillis / millisecondsToDays;
+    
+    return differenceInDays;
+  }
+
+  // this one will help get the user input 
+  // let userDate = document.getElementById('')
+
+  let daysPassed = calculateDifferenceinDays(originDate, userDate);
+  
+  console.log(daysPassed); // will print the number of days between the origin date and given date
+  phasesPast = Math.trunc(daysPassed/LENGTHOFCYCLE)
+  cycleRemainder = daysPassed % LENGTHOFCYCLE;
+
+
+  // switch determines which phase matches the completion percentage
+  switch (true)
+  {
+    case (cycleRemainder < 12.5):
+        phase = "Full Moon";
+        console.log("It is printing");
+        break;
+    case (cycleRemainder < 25 && cycleRemainder >= 12.5):
+        phase = "Waning Gibbous";
+        break;
+    case (cycleRemainder < 37.5 && cycleRemainder >= 25):
+        phase = "Third Quarter";
+        break;
+    case (cycleRemainder < 50 && cycleRemainder >= 37.5):
+        phase = "Waning Crescent";
+        break;
+    case (cycleRemainder < 62.5 && cycleRemainder >= 50):
+        phase = "New Moon";
+        break;
+    case (cycleRemainder < 75 && cycleRemainder >= 62.5):
+        phase = "Waxing Crescent";
+        break;
+    case (cycleRemainder < 87.5 && cycleRemainder >= 75):
+        phase = "First Quarter";
+        break;
+    case (cycleRemainder >= 87.5):
+        phase = "Waxing Gibbous";
+        break;
+  }
+
+console.log("the moon is "+ cycleRemainder +"% complete in its cycle");
+console.log("The moon is in the " + phase + " phase.");
+
+
